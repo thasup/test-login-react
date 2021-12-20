ef99488d6a5448bbbe3b3311b7245aae2eba1438
thasup <thanachonfirst@hotmail.com>
2021-12-21 02:04:09 +0700

    React complex state practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,11 +10,37 @@ function App() {
 
     function handleMouseOver() {
         setMouseOver(true);
-    }
+    };
 
     function handleMouseOut() {
         setMouseOver(false);
-    }
+    };
+
+    function handleChange(event) {
+        const {value, name} = event.target;
+
+        setContact(prevValue => {
+            if (name === "fName") {
+                return {
+                    fName: value,
+                    lName: prevValue.lName,
+                    email: prevValue.email
+                }
+            } else if (name === "lName") {
+                return {
+                    fName: prevValue.fName,
+                    lName: value,
+                    email: prevValue.email
+                }
+            } else if (name === "email") {
+                return {
+                    fName: prevValue.fName,
+                    lName: prevValue.lName,
+                    email: value
+                }
+            }
+        });
+    };
 
     return (
         <div className="container">
@@ -23,9 +49,24 @@ function App() {
         </h1>
         <p>{contact.email}</p>
         <form>
-            <input name="fName" placeholder="First Name" />
-            <input name="lName" placeholder="Last Name" />
-            <input name="email" placeholder="Email" />
+            <input 
+                onChange={handleChange}
+                name="fName" 
+                placeholder="First Name"
+                value={contact.fName}
+            />
+            <input 
+                onChange={handleChange}
+                name="lName" 
+                placeholder="Last Name"
+                value={contact.lName} 
+            />
+            <input 
+                onChange={handleChange}
+                name="email" 
+                placeholder="Email"
+                value={contact.email}
+            />
             <button 
                 style={{backgroundColor: isMouseOver ? "black" : "white"}}
                 onMouseOver={handleMouseOver}
